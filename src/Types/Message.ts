5e1c9ca87844b009ec9e8bb4fab23d7767e3b06e
d <d@d1c.ru>
2023-01-14 20:55:23 +0300

    Env tuning: lint fixes, node version, npm lock, ignores

--- src/Types/Message.ts
+++ src/Types/Message.ts
@@ -145,7 +145,7 @@ export type AnyRegularMessageContent = (
         listReply: Omit<proto.Message.IListResponseMessage, 'contextInfo'>
     }
     | {
-        product: WASendableProduct,
+        product: WASendableProduct
         businessOwnerJid?: string
         body?: string
         footer?: string
